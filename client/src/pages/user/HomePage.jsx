063f0c2b319045698c04d3021e56f3f53738d1a4
ramjithkr <ramjithkr5441@gmail.com>
2024-09-08 14:19:52 +0530

    26 commit

--- client/src/pages/user/HomePage.jsx
+++ client/src/pages/user/HomePage.jsx
@@ -1,6 +1,23 @@
 
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, incrementByAmount } from "../../redux/features/counterSlice";
+
 export const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+    const { value } = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
+
+    console.log("count===", value);
+
+    return (
+        <div className="px-20">
+            <h2> HomePage </h2>
+
+            <div className="flex gap-3 my-5">
+                <button onClick={()=>dispatch(decrement())} className="btn btn-primary"> -</button>
+                <p>{value} </p>
+                <button onClick={()=>dispatch(increment())} className="btn btn-primary"> +</button>
+                <button onClick={()=>dispatch(incrementByAmount(10))} className="btn btn-primary"> increment by 10</button>
+            </div>
+        </div>
+    );
+};
